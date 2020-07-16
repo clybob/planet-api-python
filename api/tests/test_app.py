8a6e2ea0b8751a3fde9725fa3af8f1490d18c896
Romulo Tavares <romuloctavares@gmail.com>
2020-07-16 18:26:36 -0300

    Cria testes para garantir que métodos de put e delete não sejam feitos na listagem de planetas.

--- api/tests/test_app.py
+++ api/tests/test_app.py
@@ -8,6 +8,16 @@ class TestPlanetsView(unittest.TestCase):
     def setUp(self):
         self.test_app = app.test_client()
 
+    def test_put_planets_should_return_not_implemented(self):
+        self.response = self.test_app.put('/planets')
+        self.assertEqual(self.response.status_code, 405)
+
+    def test_delete_planets_should_return_not_implemented(self):
+        self.response = self.test_app.delete('/planets')
+        self.assertEqual(self.response.status_code, 405)
+
     def test_get_planets_should_return_ok(self):
         self.response = self.test_app.get('/planets')
         self.assertEqual(self.response.status_code, 200)
+
+
